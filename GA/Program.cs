eafee015eb38bff6b1f813939ec975339dde208d
Bartosz Grabowski <bartek.grabowski.280@gmail.com>
2023-06-13 11:55:00 +0200

    Amend

---
+++ GA/Program.cs
@@ -0,0 +1,19 @@
+﻿using GA;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm(
+            populationSize: 50,
+            chromosomeLength: 8,
+            crossoverProbability: 0.8,
+            mutationProbability: 0.05,
+            fitnessFunction: x => x * x
+        );
+
+        double result = geneticAlgorithm.Run(numGenerations: 100);
+
+        Console.WriteLine("Result: " + result);
+    }
+}
